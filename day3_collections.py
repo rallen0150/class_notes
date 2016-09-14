d3c9ed24e4343f38b70edc54023741061d9de8da
rallen0150 <rallen0150@gmail.com>
2016-09-14 12:00:17 -0400

    added lists and dictionaries

---
+++ day3_collections.py
@@ -0,0 +1,25 @@
+
+my_list = ["Robbie", 23, ["the dark", "wasps"]]
+
+print(my_list)
+my_list.pop()# Takes off from the end
+my_list.append("peanut")# Adds to the end
+print(my_list)
+print(my_list[0])
+print(my_list[2])
+print(my_list[2][0])
+
+# Now Dictionary
+
+my_dictionary = {
+    "name": "Robbie",
+    "age": 23,
+    "fears": ["the dark", "wasps"]
+}
+
+print(my_dictionary)
+print(my_dictionary["name"])
+print(my_dictionary["age"])
+print(my_dictionary["fears"])
+
+print(my_dictionary.keys())
