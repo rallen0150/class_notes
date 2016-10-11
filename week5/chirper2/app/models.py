12c57d9e01f133aafb3378dab8c5b59aceadebed
rallen0150 <rallen0150@gmail.com>
2016-10-11 12:16:59 -0400

    user input to post on board

---
+++ week5/chirper2/app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Chirp(models.Model):
+    body = models.CharField(max_length=140)
+    created = models.DateTimeField(auto_now_add=True)
