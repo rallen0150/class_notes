a4ee4f57604744da8b6a52710bf4007f7e761467
rallen0150 <rallen0150@gmail.com>
2016-10-12 12:02:30 -0400

    got a login and logout

--- week5/chirper2/chirper2/urls.py
+++ week5/chirper2/chirper2/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 from app.views import index_view, about_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', index_view, name="index_view"),
     url(r'^about/$', about_view, name="about_view")
 ]
