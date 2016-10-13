c9d92d8f50b4c411dfc56be17f8a39d7824598f6
rallen0150 <rallen0150@gmail.com>
2016-10-13 11:59:41 -0400

    added the create and update view

--- week5/chirper2/app/models.py
+++ week5/chirper2/app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Chirp(models.Model):
     body = models.CharField(max_length=140)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created", )
