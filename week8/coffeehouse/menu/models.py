2cbcf914c570cd01305a1f291c8a452f5c6a451f
rallen0150 <rallen0150@gmail.com>
2016-10-31 13:30:32 -0400

    finished up beginning of image storage

--- week8/coffeehouse/menu/models.py
+++ week8/coffeehouse/menu/models.py
@@ -6,3 +6,10 @@ class Special(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
+    picture = models.FileField()
+
+    @property
+    def image_url(self):
+        if self.picture:
+            return self.picture.url
+        return "http://greenwoodcalendar.com/wp-content/uploads/2014/09/mystery-box.jpg"
--- week8/coffeehouse/menu/views.py
+++ week8/coffeehouse/menu/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from menu.models import Special
@@ -10,10 +10,15 @@ class SpecialListView(ListView):
 
 class SpecialCreateView(CreateView):
     model = Special
-    fields = ('title', 'description')
+    fields = ('title', 'description', 'picture')
     success_url = reverse_lazy("special_list_view")
 
     def form_valid(self, form):
         instance = form.save(commit=False)
         instance.user = self.request.user
         return super().form_valid(form)
+
+class SpecialUpdateView(UpdateView):
+    model = Special
+    fields = ('title', 'description', 'picture')
+    success_url = reverse_lazy("special_list_view")
