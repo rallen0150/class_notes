575a4427032b3d756fd0380c4774ff2e9a46337b
rallen0150 <rallen0150@gmail.com>
2016-11-01 13:06:29 -0400

    started searchbar

---
+++ week8/lyrics/app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+import requests
+from bs4 import BeautifulSoup
+
+class SongView(TemplateView):
+    template_name = 'song.html'
+
+    def get_context_data(self, song_url):
+        context = super().get_context_data()
+        page = requests.get("http://metrolyrics.com/" + song_url)
+        souper = BeautifulSoup(page.text, 'html.parser')
+        context['song_lyrics'] = souper.find_all(id='lyrics-body-text')
+        # print(page.text) # for the terminal
+        return context
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        if self.request.GET:
+            base_url = 'http://api.metrolyrics.com/v1//multisearch/all/X-API-KEY/196f657a46afb63ce3fd2015b9ed781280337ea7/format/json?find={}&theme=desktop'
+            data = requests.get(base_url.format(self.request.GET.get('song_title'))).json()
+            # print(data['results'].keys()) # To find the keys of the dictionary
+            results = data['results']['songs']['d']
+            urls = []
+            for result in results:
+                urls.append(result['u'])
+            context['song_urls'] = urls
+            print('You Clicked the search button')
+        return context
