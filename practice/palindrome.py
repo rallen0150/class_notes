41d09e8d6245ab40eda56502f7fdd517badc807e
rallen0150 <rallen0150@gmail.com>
2017-02-08 12:55:43 -0500

    redoing older assignments

---
+++ practice/palindrome.py
@@ -0,0 +1,15 @@
+
+def palindrome(sentence):
+    new_sentence = sentence
+    new_sentence = new_sentence.replace(',', '').replace('.', '').replace("'", '').replace('!', '').replace('"', '').replace('?',
+    '').replace(':', '').replace(';', '').replace('&', '').replace(' ', '').lower()
+
+    if new_sentence == new_sentence[::-1]:
+        answer = "This IS a Palindrome!!!"
+    else:
+        answer = "Sorry, this is NOT a Palindrome!"
+
+    return answer
+
+sentence = input("Enter a word or phrase:\n>")
+print(palindrome(sentence))
