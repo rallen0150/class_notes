f30cd7d51bfac926ade0b7eb00db44ada2631c32
rallen0150 <rallen0150@gmail.com>
2017-02-16 11:27:14 -0500

    simple equations

--- practice/math.py
+++ practice/math.py
@@ -14,6 +14,14 @@ def cylinder_volume(radius, height):
     volume = height*pi*(radius**2)
     return volume
 
+def triangle_area(base, height):
+    area = (base*height)/2
+    return area
+
+def square_a_sum(x, y):
+    ans = (x+y)**2
+    return ans
+
 radius = input("What is the radius of the circle? ")
 radius = float(radius)
 print(circle_area(radius))
@@ -27,3 +35,15 @@ print(cylinder_volume(new_radius, height))
 side = input("Enter a side of the square: ")
 side = float(side)
 print(simple_paramater(side))
+
+base = input("What is the base of the triangle? ")
+base = float(base)
+height = input("What is the height of the triangle? ")
+height = float(height)
+print(triangle_area(base, height))
+
+x = input("Enter the first number: ")
+x = float(x)
+y = input("Enter the second number: ")
+y = float(y)
+print(square_a_sum(x, y))
