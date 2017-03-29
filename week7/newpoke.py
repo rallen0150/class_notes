d99d56b387b221c426f1c0c6fa5066ad128ad702
rallen0150 <rallen0150@gmail.com>
2017-03-29 11:08:44 -0400

    working on finding specifics

--- week7/newpoke.py
+++ week7/newpoke.py
@@ -1,12 +1,14 @@
 import requests
 
-url = "http://pokeapi.co/api/v2/pokemon/1"
+endpoint = input("What do you want to search for? (pokemon), (type), or (ability)?:\n>")
+number = input("What number do you want to see?:\n>")
+url = "http://pokeapi.co/api/v2/{}/{}/".format(endpoint, number)
 
 json_result = requests.get(url).json()
 result_list = json_result.keys()
 
 print(result_list)
 
-category = input("What info about the Pokémon would you like to see?:\n>")
+category = input("\nWhat info about the item or Pokémon would you like to see?:\n>")
 
 print(json_result[category])
