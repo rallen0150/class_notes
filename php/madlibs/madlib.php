cd600291c7a2b3afd57d363dcc3863ba69576448
rallen0150 <rallen0150@gmail.com>
2017-04-13 12:09:16 -0400

    made a madlibs in php

---
+++ php/madlibs/madlib.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Madlibs</title>
+  </head>
+  <body>
+    <?php
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! What a(n) <b>{$_POST["adj1"]}</b> phrase<br>";
+
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! Ain`t no passing craze<br>";
+
+      echo "It means no <b>{$_POST["noun1"]}</b> for the rest of your <b>{$_POST["noun2"]}</b><br>";
+
+      echo "It`s our <b>{$_POST["adj2"]}</b> philosophy<br>";
+
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>?<br>";
+
+      echo "Yeah. It`s our <b>{$_POST["noun3"]}</b>!<br>";
+
+      echo "What`s a <b>{$_POST["noun3"]}</b>?<br>";
+
+      echo "Nothing. What`s a-<b>{$_POST["noun3"]}</b> with you?<br>";
+
+      echo "Those <b>{$_POST["num1"]}</b> words will {$_POST["verb1"]}</b> all your problems<br>";
+
+      echo "That`s right. Take <b>{$_POST["name"]}</b> here<br>";
+      echo "Why, when he was a <b>{$_POST["adj3"]}</b> <b>{$_POST["noun4"]}</b>...<br>";
+
+      echo "When I was a <b>{$_POST["adj3"]}</b> <b>{$_POST["noun4"]}</b><br>";
+
+      echo "Very <b>{$_POST["adj4"]}</b><br>";
+
+      echo "Thanks<br>";
+
+      echo "He found his <b>{$_POST["noun5"]}</b> <b>{$_POST["verb2"]}</b> a certain appeal<br>";
+      echo "He could clear the/a <b>{$_POST["place"]}</b> after every <b>{$_POST["noun6"]}</b><br>";
+
+      echo "I`m a <b>{$_POST["adj5"]}</b> soul though I seem <b>{$_POST["adj6"]}</b><br>";
+      echo "And it <b>{$_POST["verb3"]}</b> that my <b>{$_POST["noun7"]}</b> never stood downwind<br>";
+      echo "And oh, the shame - He was ashamed<br>";
+      echo "Thought of changin` my <b>{$_POST["noun8"]}</b> What`s in a <b>{$_POST["noun8"]}</b>?<br>";
+      echo "And I got <b>{$_POST["verb4"]}</b> How did ya feel?<br>";
+      echo "Everytime that I...<br>";
+
+      echo "Hey! <b>{$_POST["name"]}</b>! Not in front of the <b>{$_POST["noun9"]}</b>!<br>";
+      echo "Oh. Sorry<br>";
+
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! What a <b>{$_POST["adj1"]}</b> phrase<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! Ain`t no passing craze<br>";
+
+      echo "It means no <b>{$_POST["noun1"]}</b> for the rest of your {$_POST["noun2"]}</b><br>";
+
+      echo "It`s our <b>{$_POST["adj2"]}</b> philosophy<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! <b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! <b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! <b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>! <b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+
+      echo "It means no <b>{$_POST["noun1"]}</b> for the rest of your <b>{$_POST["noun2"]}</b><br>";
+
+      echo "It`s our <b>{$_POST["adj2"]}</b> philosophy<br>";
+      echo "<b>{$_POST["funny1"]}</b> <b>{$_POST["funny2"]}</b>!<br>";
+    ?>
+  </body>
+</html>
---
+++ php/madlibs/madlibs_input.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Madlibs</title>
+  </head>
+  <body>
+    <form class="" action="madlib.php" method="post">
+      <p>Funny Word: <input type="text" name="funny1"></p>
+      <p>Funny Word: <input type="text" name="funny2"></p>
+      <p>Adjective: <input type="text" name="adj1"></p>
+      <p>Noun (Plural): <input type="text" name="noun1"></p>
+      <p>Noun (Plural): <input type="text" name="noun2"></p>
+      <p>Adjective: <input type="text" name="adj2"></p>
+      <p>Noun: <input type="text" name="noun3"></p>
+      <p>Number: <input type="number" name="num1"></p>
+      <p>Verb: <input type="text" name="verb1"></p>
+      <p>Male Name: <input type="text" name="name"></p>
+      <p>Adjective: <input type="text" name="adj3"></p>
+      <p>Noun: <input type="text" name="noun4"></p>
+      <p>Adjective: <input type="text" name="adj4"></p>
+      <p>Noun: <input type="text" name="noun5"></p>
+      <p>Verb (-ed): <input type="text" name="verb2"></p>
+      <p>Place: <input type="text" name="place"></p>
+      <p>Noun: <input type="text" name="noun6"></p>
+      <p>Adjective: <input type="text" name="adj5"></p>
+      <p>Adjective: <input type="text" name="adj6"></p>
+      <p>Verb (-ed): <input type="text" name="verb3"></p>
+      <p>Noun: <input type="text" name="noun7"></p>
+      <p>Noun: <input type="text" name="noun8"></p>
+      <p>Verb (-ed): <input type="text" name="verb4"></p>
+      <p>Noun (Plural): <input type="text" name="noun9"></p>
+      <p><input type="submit" value="Create!"></p>
+    </form>
+  </body>
+</html>
