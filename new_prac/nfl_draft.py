5e593ba218e56869a732fcdb014fea590731af2e
rallen0150 <rallen0150@gmail.com>
2017-04-27 18:23:16 -0400

    hopfully clean up how it looks, still waiting for 8 to see if works

--- new_prac/nfl_draft.py
+++ new_prac/nfl_draft.py
@@ -2,10 +2,15 @@ from bs4 import BeautifulSoup
 
 import requests
 
-url = "http://www.espn.com/nfl/draft/rounds"
+round_num = input("What round would you like to see:\n>")
+url = "http://www.espn.com/nfl/draft/rounds/_/round/{}".format(round_num)
 x = requests.get(url)
 soup = BeautifulSoup(x.text, "html.parser")
 # print(soup)
+num = 1
+
+print("\nRound {} of 2017 NFL Draft\n".format(round_num))
 
 for link in soup.find_all(id="draftcast-draftroundresults"):
-    print(link.get("draftTable__playerInfo"))
+    print(num, link.get("draftTable__playerInfo"))
+    num+=1
