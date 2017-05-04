962216de690c4b1bbbd826026cc17c65fcc55153
rallen0150 <rallen0150@gmail.com>
2017-05-04 10:51:45 -0400

    find holidays based on dates

---
+++ new_prac/datetime/holiday.py
@@ -0,0 +1,23 @@
+from datetime import date
+import holidays
+
+print(date(2014, 1, 1) in holidays.US())
+
+print("\nList of US Holidays in 2017:")
+for date, name in sorted(holidays.US(state='SC', years=2017).items()):
+    print(date, name)
+
+print("\nList of Canadian Holidays in 2017:")
+for date, name in sorted(holidays.Canada(years=2017).items()):
+    print(date, name)
+
+us_holiday = holidays.UnitedStates()
+canadian_holiday = holidays.Canada()
+
+date = input("\nEnter a date to see if that date is a Holiday (format: 'Year-Month-Day'):\n>")
+if us_holiday.get(date):
+    print(us_holiday.get(date))
+elif canadian_holiday.get(date):
+    print(canadian_holiday.get(date))
+else:
+    print("That isn't a US or Canadian Holiday!")
