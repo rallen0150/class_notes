85cc614af61ce5d38cd5668b4aa89f6a012b286b
rallen0150 <rallen0150@gmail.com>
2017-05-04 12:26:47 -0400

    added a calendar to the holiday

--- new_prac/datetime/holiday.py
+++ new_prac/datetime/holiday.py
@@ -1,5 +1,6 @@
 from datetime import date
 import holidays
+import calendar
 
 print(date(2014, 1, 1) in holidays.US())
 
@@ -11,13 +12,23 @@ print("\nList of Canadian Holidays in 2017:")
 for date, name in sorted(holidays.Canada(years=2017).items()):
     print(date, name)
 
+
 us_holiday = holidays.UnitedStates()
 canadian_holiday = holidays.Canada()
+
 
 date = input("\nEnter a date to see if that date is a Holiday (format: 'Year-Month-Day'):\n>")
 if us_holiday.get(date):
     print(us_holiday.get(date))
+    date = date.split("-")
+    year = date[0]
+    month = date[1]
+    print(calendar.month(int(year), int(month)))
 elif canadian_holiday.get(date):
     print(canadian_holiday.get(date))
+    date = date.split("-")
+    year = date[0]
+    month = date[1]
+    print(calendar.month(int(year), int(month)))
 else:
     print("That isn't a US or Canadian Holiday!")
