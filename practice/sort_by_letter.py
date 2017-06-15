d481c609a8dfd7f982805996d9b2c714add0db5e
rallen0150 <rallen0150@gmail.com>
2017-06-14 21:48:23 -0400

    fixing bugs

--- practice/sort_by_letter.py
+++ practice/sort_by_letter.py
@@ -1,10 +1,10 @@
 
 def sort_by_letter(sentence):
-    words = sentence.split(',')
+    words = sentence.split(' ')
     words.sort()
     sorted_words = ','.join(words)
     return sorted_words
 
-sentence = input("Type in words seperated by commas:\n")
+sentence = input("Type in words seperated by spaces:\n").lower()
 words = sort_by_letter(sentence)
 print(words)
