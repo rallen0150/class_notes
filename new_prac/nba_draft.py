ee8bae57c3052ca5fc318120ce3a10ba53073366
rallen0150 <rallen0150@gmail.com>
2017-06-22 10:38:18 -0400

    wanted another draftcast, but for nba

---
+++ new_prac/nba_draft.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import time
+import requests
+
+# Another scrape but for the NBA Draft 2017
+rd_num = input("Which Round of the NBA Draft do you want to see? 1 or 2?\n>")
+url = "http://www.espn.com/nba/draft/rounds/_/round/{}".format(rd_num)
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+
+print("Round {} of the 2017 NBA Draft".format(rd_num))
+
+for draftTable__headline in soup.find_all(class_="draftTable__headline"):
+    team = str(draftTable__headline.img).upper()
+    team = team[79:82] #or 69:72
+    print(team, draftTable__headline.text.replace("\n", " ").strip())
